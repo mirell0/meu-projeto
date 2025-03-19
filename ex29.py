b457724d9e087923f1260f2be7aba2a08052758a
Mirella <143643139+mirell0@users.noreply.github.com>
2025-03-19 10:00:29 -0300

    Update ex29.py

--- ex29.py
+++ ex29.py
@@ -11,3 +11,27 @@ def exibir_menu():
         turma = input("Turma")
         curso = input("Curso")
         cadastro.append({"Nome": nome, "Idade": idade, "Turma": turma, "Curso": curso })
+         print ("cadastro realizado com sucesso!")
+
+        def ver_cadastros(cadastros):
+            if not cadastros:
+                print("nenhum cadastro no sistema.")
+                else:
+                    print("/n-----------LISTA DE CADASTROS----------")
+                for i, pessoa in enumerate (cadastros, 1):
+                print (f "(i), nome:(pessoa ['nome]},Idade:
+                pessoa ['idade']}, turma : {pessoa [ 'turma']},curso : {pessoa['curso']}")
+                
+
+def main ():
+    cadastros = []
+    while true:
+        exibir_menu()
+        opção = input("Escolha uma opção:")
+        if opção == "1":
+            cadastrar_pessoa (cadastros)
+        elif opção == "2" : 
+        ver_cadastros (cadastros)
+elif opção == "3":
+print ("Obrigada por ultilizar nosso sistema")
+break
